9e45a15b2006b627c26b6ee8ace0461fae82207d
Michelle Chan <michellelychan@gmail.com>
2019-11-14 14:05:07 +0800

    set up python flask environment

--- testing.py
+++ testing.py
@@ -6,4 +6,6 @@ client = textrazor.TextRazor(extractors=["entities", "topics"])
 response = client.analyze_url("http://www.bbc.co.uk/news/uk-politics-18640916")
 
 for entity in response.entities():
-    print entity.id, entity.relevance_score, entity.confidence_score, entity.freebase_types
+    print entity.id
+
+    # entity.relevance_score, entity.confidence_score, entity.freebase_types 
