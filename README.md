60a593b1c66f89850ed33bc95ed4fd848e71c464
Michelle Chan <michellelychan@gmail.com>
2019-11-16 21:07:56 +0800

    readme update

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # keyword api app 
 This app uses Python Flask. 
+
+Only the following files are useful: 
+- app.py 
+- textrazor_tools.py 
+- index.html (it was originally linked to text-processing.js but now want to move to app.py)
+
+I still haven't linked the files up. 
+
+To run app, run app.py
--- textrazor_tools.py
+++ textrazor_tools.py
@@ -5,8 +5,8 @@ textrazor.api_key = "7b177ab63d438808a6fbd9c451b4e492499029cddd0fe02c11cbf016"
 client = textrazor.TextRazor(extractors=["entities", "topics"])
 
 
-# response = client.analyze_url("https://www.scmp.com/sport/martial-arts/mixed-martial-arts/article/3037656/hong-kongs-ufc-academy-star-ramona-pascual")
-response = client.analyze_url("https://www.scmp.com/news/hong-kong/politics/article/3038049/pla-soldiers-sent-streets-hong-kong-first-time-protests")
+response = client.analyze_url("https://www.scmp.com/sport/martial-arts/mixed-martial-arts/article/3037656/hong-kongs-ufc-academy-star-ramona-pascual")
+# response = client.analyze_url("https://www.scmp.com/news/hong-kong/politics/article/3038049/pla-soldiers-sent-streets-hong-kong-first-time-protests")
 
 
 print "Entity id | Relevance Score | Confidence Score"
