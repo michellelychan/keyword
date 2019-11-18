c5d822a1cc9c41747ab91035af4786c3942fd5b6
David Ng <david.ng@scmp.com>
2019-11-18 22:46:21 +0800

    Clean up deployment

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,2 @@
-export PYTHONPATH=.
-export FLASK_APP=web
-apt-get update -qy
-apt-get install -y python3-dev python3-pip
-pip3 install Flask gunicorn pytest pytest-cov
-pip3 install -r requirements
-
 gunicorn --bind=0.0.0.0:9060 --daemon reload:app
-gunicorn --bind 0.0.0.0:9061 --reload app:app --limit-request-field_size 0 --limit-request-line 0
+gunicorn --bind 0.0.0.0:9061 --reload main:app --limit-request-field_size 0 --limit-request-line 0
